4ae38a3cd166735c8452c65448837a9b88e79eda
HarpreetSinghChouhan <happyubxty@gmail.com>
2026-04-06 12:02:41 +0530

    update artical page

--- frontend/app/articles/[id]/page.tsx
+++ frontend/app/articles/[id]/page.tsx
@@ -1,1 +1,12 @@
-  
+import { Typography } from "@mui/material";
+
+  export default function Articals({params}:{params:{id:String | null}}){
+    return(
+        <>
+        <Typography >
+             Wellcome one Artical page 
+        </Typography>
+        
+        </>
+    )
+  }
